efb00294f9aaa3144ff3672b3e5d049a5797f87a
angiecortez <angiecorteztay1@hotmail.com>
2018-08-07 11:24:50 -0500

    cambiando de api, agregando json, la informacion sale en los puntos

---
+++ src/main.js
@@ -0,0 +1,60 @@
+// Set the map variable
+const myMap = L.map('map');
+
+// Load the basemap
+const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+// Add basemap to map id
+myBasemap.addTo(myMap);
+
+// Set view of the map
+myMap.setView([-12.0570522, -76.9418717], 12);
+// myMap.setView([-12.1151394, -77.0118109], 15);
+
+// Make an XMLHttpRequest to the JSON data
+const request = new XMLHttpRequest();
+request.open('GET', 'data.JSON', true);
+request.onload = function() {
+  // Begin accessing JSON data here
+  const data = JSON.parse(this.response);
+
+  // Reduce neighborhoods down to how many they are, and count them
+    const neighborhoodCount = data.foods.reduce((sums, food) => {
+    sums[food.neighborhood] = (sums[food.neighborhood] || 0) + 1;
+    return sums;
+    }, {});
+
+    // Create a sidebar
+    const sidebar = document.getElementById('neighborhoods');
+    const h3 = document.createElement("h3");
+    h3.innerHTML = "Total de lugares";
+    sidebar.appendChild(h3);
+
+    // Print all neighborhoods in sidebar
+    for (let neighborhood in neighborhoodCount) {
+    const p = document.createElement("p");
+    p.innerHTML = `<b>${neighborhood}</b> : ${neighborhoodCount[neighborhood]}`;
+    sidebar.appendChild(p);
+    }
+
+    // Print food markers
+    const foods = data.foods.map(food => {
+    L.marker([food.lat, food.long]).bindPopup(`
+        <div class="container">
+        <img src="${food.foto}" class="w-100 border border-light mt-10" alt="hola">
+        <h2>${food.name}</h2>
+        <p><strong>Distrito:</strong> ${food.neighborhood}</p>
+        <p><strong>Ambiente:</strong> ${food.ambiance}</p>
+        <p><strong>Sabor:</strong> ${food.flavor}</p>
+        <p><strong>Comentarios:</strong> ${food.comments}</p>
+        <button class="btn btn-primary">Pedir ahora</button>
+
+        </div>
+    `).openPopup().addTo(myMap);
+    });
+}
+
+request.send();
